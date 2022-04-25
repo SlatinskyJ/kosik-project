b3322d41b5282a6b49f91c67efc8809ae6e6558c
SlatinskyJ <jakub.slatinsky@gmail.com>
2022-04-25 11:31:15 +0200

    Added Mui theme

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -2,12 +2,26 @@ import React, { ReactElement } from 'react';
 import './App.css';
 import MyRoutes from '../MyRoutes/MyRoutes';
 import PageHeader from '../PageHeader/PageHeader';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function App(): ReactElement {
+	const theme = createTheme({
+		palette: {
+			primary: {
+				main: '#0052cc',
+			},
+			secondary: {
+				main: '#edf2ff',
+			},
+		},
+	});
+
 	return (
 		<div className="App">
-			<PageHeader />
-			<MyRoutes />
+			<ThemeProvider theme={theme}>
+				<PageHeader />
+				<MyRoutes />
+			</ThemeProvider>
 		</div>
 	);
 }
